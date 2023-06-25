dc2d190347101b66ba216ab0478f7bca8201909a
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2023-06-25 19:08:59 -0400

    Add files via upload

--- nudo.bash
+++ nudo.bash
@@ -7,12 +7,12 @@
 ## Run command as a normal user instead of root, while still using root's files
 if [[ $@ ]]; then
     if [ "`id -u`" -eq 0 ]; then 
-        adduser --no-create-home --gecos --disabled-password --disabled-login "nudo"
+        adduser --no-create-home --home /root --gecos --disabled-password --disabled-login "nudo"
     else
         printf "Why bother running as a normal user when you are one?\n" 
     fi 
-        sudo --login --user "nudo" $@
-        userdel "nudo"
+        sudo --user "nudo" $@
+        userdel --force "nudo" ; printf "Deleted nudo\n"
 else
     printf "ADD A PROGRAM AS ARGUMENT\nFor example: $ nudo.bash /home/user/scripts/a-script.sh\n"
 fi
